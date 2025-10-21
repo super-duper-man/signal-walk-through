1fc6d2002a203f322c76005c52e14bcfd9573241
Danny <m.r.fouladi7@gmail.com>
2025-10-21 13:38:17 +0330

    add contact loginc

--- src/app/components/add-contact/add-contact.component.ts
+++ src/app/components/add-contact/add-contact.component.ts
@@ -1,36 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { RouterLinkWithHref } from '@angular/router';
+import { Router, RouterLinkWithHref } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, RouterLinkWithHref],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, RouterLinkWithHref, FormsModule, MatProgressSpinner],
   template: `
     <div class="container">
       <h2>Add Contact</h2>
-      <form>
+      <form (ngSubmit)="addContact()">
         <div class="fields">
           <mat-form-field>
             <mat-label>Name</mat-label>
-            <input matInput />
+            <input matInput [(ngModel)]="name" name="name" />
           </mat-form-field>
           <mat-form-field>
             <mat-label>Email</mat-label>
-            <input matInput />
+            <input matInput [(ngModel)]="email" name="email" />
           </mat-form-field>
           <mat-form-field>
             <mat-label>Phone</mat-label>
-            <input matInput />
+            <input matInput [(ngModel)]="phone" name="phone" />
           </mat-form-field>
         </div>
 
         <div class="actions">
-          <button mat-flat-button>Save</button>
-          <button mat-raised-button routerLink="/">Cancel</button>
+          <button type="submit" mat-flat-button>Save</button>
+          <button type="button" mat-raised-button routerLink="/">Cancel</button>
         </div>
       </form>
+      @if(saving()){
+        <mat-progress-spinner mode="indeterminate" />
+      }
     </div>
   `,
   styles: `
@@ -50,4 +56,24 @@ import { RouterLinkWithHref } from '@angular/router';
     }
   `,
 })
-export class AddContactComponent {}
+export class AddContactComponent {
+  private apiService = inject(ApiService);
+  private router = inject(Router);
+
+  name = signal('');
+  email = signal('');
+  phone = signal('');
+  saving = signal(false);
+
+  async addContact() {
+    this.saving.set(true);
+    await this.apiService.addContact({
+      id: '123dasd',
+      name: this.name(),
+      email: this.email(),
+      phone: this.phone(),
+    });
+    this.saving.set(false);
+    this.router.navigateByUrl('/');
+  }
+}
